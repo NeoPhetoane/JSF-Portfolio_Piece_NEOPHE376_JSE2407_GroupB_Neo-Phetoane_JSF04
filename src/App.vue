c529e833dc4196f4a4ed8366b4910744b91f9c81
NeoPhetoane <phetoaneneo4@gmail.com>
2024-08-06 09:18:07 +0200

    Home component under views, handles what initially loads on the page. Fetches products from API and has loading state.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from './components/Header.vue';
+import Home from './views/Home.vue';
 </script>
 
 
 
 <template>
    <Header />
+   <Home />
 </template>
 
 <style>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mx-auto p-4">
+    <div v-if="loading" class="text-center">Loading...</div>
+    <div
+      v-else
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [],
+      loading: true,
+    };
+  },
+  methods: {
+    fetchProducts() {
+      fetch("https://fakestoreapi.com/products")
+        .then((response) => response.json())
+        .then((data) => {
+          this.products = data;
+          this.loading = false;
+        });
+    },
+  },
+};
+</script>
