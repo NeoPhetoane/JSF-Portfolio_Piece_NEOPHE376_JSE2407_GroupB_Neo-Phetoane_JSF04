619509111c423f186d4ed6a42cce25dbe4be236a
NeoPhetoane <phetoaneneo4@gmail.com>
2024-08-06 11:49:33 +0200

    Path to detail page added to ProductCard.

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="border p-4 rounded-lg">
+      <router-link :to="`/product/${product.id}`">
         <img :src="product.image" alt="Product image" class="w-full" />
         <h2 class="text-lg">{{ product.title }}</h2>
         <p>Price: ${{ product.price }}</p>
         <p>Category: {{ product.category }}</p>
+      </router-link>
     </div>
   </template>
   
