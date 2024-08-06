de4ea5380c4df5d2899427b74d2efb4901ada180
NeoPhetoane <phetoaneneo4@gmail.com>
2024-08-06 09:32:28 +0200

    FilterSort component added. The display dropdowns along with logic of what to extract from API.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 import Header from './components/Header.vue';
 import Home from './views/Home.vue';
+import FilterSort from './components/FilterSort.vue';
 </script>
 
 
 
 <template>
    <Header />
+<FilterSort/>
    <Home />
+
 </template>
 
 <style>
---
+++ src/components/FilterSort.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div class="mb-4">
+        <label for="category" class="block text-sm font-medium text-gray-700">Filter by Category:</label>
+      <select @change="filter($event)" class="mr-4">
+        <option value="">All Categories</option>
+        <option v-for="category in categories" :key="category" :value="category">{{ category }}</option>
+      </select>
+      <label for="sort" class="block text-sm font-medium text-gray-700">Sort by Price:</label>
+      <select @change="sort($event)">
+        <option value="default">Default</option>
+        <option value="lowest">Lowest Price</option>
+        <option value="highest">Highest Price</option>
+      </select>
+    </div>
+  </template>
+
+
+<script>
+
+</script>
