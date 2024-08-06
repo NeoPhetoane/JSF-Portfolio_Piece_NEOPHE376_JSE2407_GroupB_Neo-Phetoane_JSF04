a9a38801c31d3c3b0f80659cdb73df2d2aa2f50f
NeoPhetoane <phetoaneneo4@gmail.com>
2024-08-06 10:45:58 +0200

    Filtering and Sorting logic added to Home to manage the display of products once they are applied.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,18 @@
       v-else
       class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
     >
-      <ProductCard v-for="product in products" :key="product.id" :product="product" />
+      <ProductCard
+        v-for="product in filteredProducts"
+        :key="product.id"
+        :product="product"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import ProductCard from '../components/ProductCard.vue';
+import ProductCard from "../components/ProductCard.vue";
+import FilterSort from "../components/FilterSort.vue";
 
 export default {
   components: { ProductCard },
@@ -21,24 +26,48 @@ export default {
       products: [],
       loading: true,
       error: null,
+      filteredProducts: [],
+      categories: [],
+      selectedCategory: "",
+      sortOrder: "default",
     };
   },
   methods: {
-    async fetchProducts() {
-      try {
-        const response = await fetch("https://fakestoreapi.com/products");
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        this.products = data;
-      } catch (error) {
-        this.error = "Error fetching products: " + error.message;
-      } finally {
-        this.loading = false;
+    fetchProducts() {
+      fetch("https://fakestoreapi.com/products")
+        .then((response) => response.json())
+        .then((data) => {
+          this.products = data;
+          this.applyFiltersAndSorting();
+          this.loading = false;
+        });
+    },
+    filterProducts(category) {
+      this.selectedCategory = category;
+      this.applyFiltersAndSorting();
+    },
+    sortProducts(order) {
+      this.sortOrder = order;
+      this.applyFiltersAndSorting();
+    },
+    applyFiltersAndSorting() {
+      let filtered = this.products;
+      if (this.selectedCategory) {
+        filtered = filtered.filter(
+          (product) => product.category === this.selectedCategory
+        );
       }
+      if (this.sortOrder === "lowest") {
+        filtered.sort((a, b) => a.price - b.price);
+      } else if (this.sortOrder === "highest") {
+        filtered.sort((a, b) => b.price - a.price);
+      } else if (this.sortOrder === "default") {
+        filtered.sort((a, b) => a.id - b.id);
+      }
+      this.filteredProducts = filtered;
     },
   },
+
   mounted() {
     this.fetchProducts();
   },
