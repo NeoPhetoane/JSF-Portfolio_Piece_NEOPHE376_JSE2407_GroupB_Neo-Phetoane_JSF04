e9fa2cbeb1063f121c6cd9ac4726c0bbd1e6f9af
NeoPhetoane <phetoaneneo4@gmail.com>
2024-08-15 13:33:01 +0200

    Login route added to router.js

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './views/Home.vue';
 import ProductDetail from './views/ProductDetail.vue';
+import Login from "./views/Login.vue";
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     path: '/product/:id',
     name: 'ProductDetail',
     component: ProductDetail,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
 ];
 
